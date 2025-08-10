fc5d0dee0287053a42841f9f9c72e1ebd5b3bc26
Bruce Lau <liulqx@gmail.com>
2025-08-11 01:31:06 +0800

    feat: go eth

--- test/2508/14_go_eth_balance.go
+++ test/2508/14_go_eth_balance.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -12,33 +13,49 @@ import (
 
 func main() {
 	//client, err := ethclient.Dial("https://cloudflare-eth.com")
+	//client, err := ethclient.Dial("http://127.0.0.1:8545")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
 	client, err := ethclient.Dial("http://127.0.0.1:8545")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	//account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	//account := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+	//balance, err := client.BalanceAt(context.Background(), account, nil)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(balance, "WEI") // 25893860171173005034
+	//
+	////blockNumber := big.NewInt(5532993)
+	//blockNumber := big.NewInt(0)
+	//balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(balanceAt, "WEI") // 25729324269165216042
+	//
+	//fbalance := new(big.Float)
+	//fbalance.SetString(balanceAt.String())
+	////ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	//ethValue := new(big.Float).Quo(fbalance, big.NewFloat(1e18))
+	//fmt.Println(ethValue, "ETH") // 25.729324269165216041
+	//
+	//pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	//fmt.Println(pendingBalance, "WEI") // 25729324269165216042
+
 	account := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-	balance, err := client.BalanceAt(context.Background(), account, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(balance, "WEI") // 25893860171173005034
+	balance, err := client.BalanceAt(context.Background(), account, big.NewInt(0))
+	fmt.Println(balance)
 
-	//blockNumber := big.NewInt(5532993)
-	blockNumber := big.NewInt(0)
-	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(balanceAt, "WEI") // 25729324269165216042
+	floatBalance := new(big.Float)
+	floatBalance.SetString(balance.String())
 
-	fbalance := new(big.Float)
-	fbalance.SetString(balanceAt.String())
-	//ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
-	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(1e18))
-	fmt.Println(ethValue, "ETH") // 25.729324269165216041
+	ethValue := new(big.Float).Quo(floatBalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue)
 
-	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
-	fmt.Println(pendingBalance, "WEI") // 25729324269165216042
 }
